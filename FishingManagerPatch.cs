2072a1503b217591e9042c9b3f6fdff6681f1134
zonde306 <13820323+zonde306@users.noreply.github.com>
2022-07-28 19:28:25 +0800

    init

---
+++ FishingManagerPatch.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+
+namespace RF5_AutoFishing
+{
+	[HarmonyPatch(typeof(Fishing.FishingManager), nameof(Fishing.FishingManager.FishHit))]
+	public class FishingManagerPatch
+	{
+		static void Postfix(Fishing.FishingManager __instance)
+		{
+			__instance.GetFish();
+		}
+	}
+}
---
+++ Main.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using HarmonyLib;
+using BepInEx.IL2CPP;
+using BepInEx.Logging;
+
+namespace RF5_AutoFishing
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+    [BepInProcess(GAME_PROCESS)]
+    public class Main : BasePlugin
+    {
+        #region PluginInfo
+        private const string GUID = "A8262A85-88AF-873E-E27A-DD44C5CE423A";
+        private const string NAME = "RF5_AutoFishing";
+        private const string VERSION = "1.0";
+        private const string GAME_PROCESS = "Rune Factory 5.exe";
+        #endregion
+
+        public override void Load()
+        {
+            new Harmony(GUID).PatchAll();
+        }
+    }
+}
