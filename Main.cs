f14c8b1807c0709a1984f50f58d14568ef8454bc
David <davidthemaster30@hotmail.com>
2025-01-03 00:57:52 -0500

    add loading logging

--- Main.cs
+++ Main.cs
@@ -10,12 +10,15 @@ namespace RF5_AutoFishing;
 public class Main : BasePlugin
 {
 
-    public static new ManualLogSource Log;
+    internal static readonly ManualLogSource Log = BepInEx.Logging.Logger.CreateLogSource("AutoFishing");
     private const string GAME_PROCESS = "Rune Factory 5.exe";
 
     public override void Load()
     {
-        Log = base.Log;
+        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_NAME} {MyPluginInfo.PLUGIN_VERSION} is loading!");
+
         new Harmony(MyPluginInfo.PLUGIN_GUID).PatchAll();
+
+        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_NAME} {MyPluginInfo.PLUGIN_VERSION} is loaded!");
     }
 }
