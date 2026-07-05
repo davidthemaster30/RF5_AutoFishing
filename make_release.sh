fa54fe6b4c7afde0ad608c5740acb090aaca1102
David Tremblay <davidthemaster30@hotmail.com>
2026-07-04 22:43:17 -0400

    Add git release to release script

--- make_release.sh
+++ make_release.sh
@@ -1,6 +1,20 @@
 rm -rf obj
 rm -rf bin
 
-dotnet build RF5_AutoFishing.csproj -f net6.0 -c Release
+CSPROJ_PATH="RF5_AutoFishing.csproj"          # path to your .csproj
 
-zip -j 'RF5_AutoFishing_v1.1.1.zip' './bin/Release/net6.0/RF5_AutoFishing.dll'
+dotnet build $CSPROJ_PATH -f net6.0 -c Release
+
+VERSION=$(grep -oP '(?<=<Version>)[^<]+' "$CSPROJ_PATH" || true)
+PROJECT_NAME=$(grep -oP '(?<=<AssemblyName>)[^<]+' "$CSPROJ_PATH" || true)
+ZIP_NAME="$PROJECT_NAME_v$VERSION.zip"
+
+zip -j "$ZIP_NAME" './bin/Release/net6.0/RF5_AutoFishing.dll'
+
+git tag "v$VERSION"
+git push origin "v$VERSION"
+ 
+gh release create "v$VERSION" "$ZIP_NAME" \
+  --title "v$VERSION" \
+  --generate-notes
+ 
